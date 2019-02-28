9dfd2b566dbd5d5bee734184dc7720f93b9299cf
universse <phuoc317049@gmail.com>
2019-02-28 20:38:54 +0800

    Fix window check

--- packages/client/src/utils/localStorageUtils.js
+++ packages/client/src/utils/localStorageUtils.js
@@ -1,9 +1,11 @@
 import LocalStorage from 'constants/LocalStorage'
 
 export const isNewUser = () =>
+  typeof window === 'object' &&
   window.localStorage.getItem(LocalStorage.IS_NEW_USER) === 'true'
 
 export const hasSignedIn = () =>
+  typeof window === 'object' &&
   window.localStorage.getItem(LocalStorage.HAS_SIGNED_IN) === 'true'
 
 export default hasSignedIn
