b58697111059145c39bf4fb4aa3b2849a6369dc9
universse <phuoc317049@gmail.com>
2019-06-23 00:17:44 +0800

    Refactor Collections style

--- packages/client/src/components/Collections/Collections.js
+++ packages/client/src/components/Collections/Collections.js
@@ -1,37 +1,15 @@
 import React, { useCallback, useRef } from 'react'
-import { css } from '@emotion/core'
 import { FixedSizeList as List } from 'react-window'
 import WindowScroller from 'react-virtualized/dist/commonjs/WindowScroller'
 
 import Item from './Item'
 import { collectionHeightInRem } from './styled'
-import { baseFontSize, screens } from 'constants/Styles'
+import { baseFontSize } from 'constants/Styles'
 import { CollectionIdsType } from 'constants/Types'
 
 function itemKey (index, data) {
   return data[index].id
 }
-
-const listStyle =
-  typeof window === 'object' &&
-  css`
-    height: 100% !important;
-    min-height: ${window.innerHeight}px;
-    overflow: initial !important;
-    width: 100%;
-
-    & > ul:not(:empty) {
-      box-shadow: var(--shadows-03);
-
-      li:last-child > div {
-        border-bottom: 1px solid transparent;
-      }
-
-      ${screens.desktop} {
-        border-radius: 8px;
-      }
-    }
-  `
 
 export default function Collections ({ collectionIds }) {
   const listRef = useRef()
@@ -45,7 +23,7 @@ export default function Collections ({ collectionIds }) {
       {({ height }) => (
         <List
           ref={listRef}
-          css={listStyle}
+          className='Collections'
           height={height}
           innerElementType='ul'
           itemCount={collectionIds.length}
