8b704aafcd4147d351afbc2ce26da18d711b2985
universse <phuoc317049@gmail.com>
2019-07-10 04:07:52 +0800

    Make Landing responsive

--- packages/client/src/assets/illustrations/HeroImage.js
+++ packages/client/src/assets/illustrations/HeroImage.js
@@ -4,10 +4,10 @@ export function HeroImage () {
   return (
     <svg
       aria-label='hero image'
-      height='400'
+      height='384'
       role='img'
       viewBox='0 0 1200 1200'
-      width='400'
+      width='384'
       xmlns='http://www.w3.org/2000/svg'
     >
       <title>hero image</title>
--- packages/client/src/components/Hero/Landing.js
+++ packages/client/src/components/Hero/Landing.js
@@ -4,6 +4,7 @@ import { css } from '@emotion/core'
 import { HeroImage } from 'assets/illustrations'
 import { PrimaryButton } from 'components/common'
 import AriaLabels from 'constants/AriaLabels'
+import { screens } from 'constants/Styles'
 import { logClickCTA } from 'utils/amplitude'
 import { scrollToHero } from '../../../gatsby/utils'
 
@@ -14,36 +15,51 @@ function Landing () {
       css={css`
         align-items: center;
         display: flex;
-        height: 28rem;
         justify-content: space-between;
+        padding: 3rem 1rem;
+
+        ${screens.nonDesktop} {
+          flex-direction: column;
+          padding: 2rem 1rem 1rem;
+        }
       `}
     >
-      <div>
-        <hgroup>
-          <h1
-            css={css`
-              color: var(--black900);
-              font-family: Spectral, serif;
-              font-size: 3rem;
-              font-weight: 800;
-              line-height: 3.5rem;
-            `}
-          >
-            Follow your curiosity.
-          </h1>
-          <h2
-            css={css`
-              color: var(--black900);
-              font-size: 1.125rem;
-              line-height: 2rem;
-            `}
-          >
-            Discover the best learning resources, curated by the community.
-          </h2>
-        </hgroup>
+      <div
+        css={css`
+          ${screens.tablet} {
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+          }
+        `}
+      >
+        <h1
+          css={css`
+            color: var(--black900);
+            font-family: Spectral, serif;
+            font-size: 3rem;
+            font-weight: 800;
+            line-height: 3.5rem;
+          `}
+        >
+          Follow your curiosity.
+        </h1>
+        <h2
+          css={css`
+            color: var(--black900);
+            font-size: 1.125rem;
+            line-height: 2rem;
+          `}
+        >
+          Discover the best learning resources, curated by the community.
+        </h2>
         <div
           css={css`
-            margin-top: 2rem;
+            margin-top: 1rem;
+
+            ${screens.desktop} {
+              margin-top: 2rem;
+            }
           `}
         >
           <PrimaryButton
