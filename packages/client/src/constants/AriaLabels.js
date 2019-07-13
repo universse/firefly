ecebb7fa9527cf5f9261e6923972be35ac6dc4e2
universse <phuoc317049@gmail.com>
2019-07-14 01:01:20 +0800

    Change search bar message

--- packages/client/src/constants/AriaLabels.js
+++ packages/client/src/constants/AriaLabels.js
@@ -3,7 +3,7 @@ export default {
   EXPLORE: 'Explore Learning Collections',
   FILTER_BY_TAGS: 'Filter by Tags',
   GO_BACK: 'Go Back to Previous Page',
-  SEARCH_BAR_LABEL: 'What Do You Want to Learn Today?',
+  SEARCH_BAR_LABEL: 'Learn Something New Today',
   SIGNIN_REGISTER: 'Sign In or Register',
   SORT_AND_FILTER_COLLECTIONS: 'Sort and Filter Collections',
   SORT_BY: 'Sort by'
