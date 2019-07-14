07881990817e0f72055f6d2142dba357a1c38d50
universse <phuoc317049@gmail.com>
2019-07-15 01:25:40 +0800

    Change search bar message

--- packages/client/src/constants/AriaLabels.js
+++ packages/client/src/constants/AriaLabels.js
@@ -2,8 +2,8 @@ export default {
   CLEAR_SEARCH_INPUT: 'Clear Search Input',
   EXPLORE: 'Explore Learning Collections',
   FILTER_BY_TAGS: 'Filter by Tags',
-  GO_BACK: 'Go Back to Previous Page',
-  SEARCH_BAR_LABEL: 'Learn Something New Today',
+  GO_BACK: 'Go back to previous page',
+  SEARCH_BAR_LABEL: 'Learn something new today!',
   SIGNIN_REGISTER: 'Sign In or Register',
   SORT_AND_FILTER_COLLECTIONS: 'Sort and Filter Collections',
   SORT_BY: 'Sort by'
