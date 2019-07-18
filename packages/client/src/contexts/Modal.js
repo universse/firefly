7cef1c5d149ee1afa56120316677bce2e3525482
universse <phuoc317049@gmail.com>
2019-07-18 22:59:30 +0800

    Lock scroll manually when modal is open

--- packages/client/src/contexts/Modal.js
+++ packages/client/src/contexts/Modal.js
@@ -1,12 +1,32 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
+import { MediaContext } from 'contexts/Media'
 import { SetModalContext } from './SetModal'
 
 export const ModalContext = createContext()
 
 export default function Modal ({ children }) {
   const [activeModalType, setActiveModalType] = useState()
+
+  const { isDesktop } = useContext(MediaContext)
+
+  useEffect(() => {
+    if (!isDesktop || !activeModalType) return
+
+    const lockWheel = e => e.preventDefault()
+    const lockButtonScroll = e => {
+      !['INPUT', 'TEXTAREA'].includes(e.target.tagName) && e.preventDefault()
+    }
+
+    window.addEventListener('keydown', lockButtonScroll)
+    window.addEventListener('wheel', lockWheel)
+
+    return () => {
+      window.removeEventListener('keydown', lockButtonScroll)
+      window.removeEventListener('wheel', lockWheel)
+    }
+  }, [activeModalType, isDesktop])
 
   useEffect(() => {
     const closeModal = () => setActiveModalType(null)
