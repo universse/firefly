dfad6df5ff0ab46b72e8cb75d77642c611a8fb77
universse <phuoc317049@gmail.com>
2019-08-09 00:09:32 +0800

    Deploy

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,6 +1,5 @@
 const shell = require('shelljs')
 
-shell.exec('yarn workspace client clean')
 shell.exec('yarn workspace netlify-functions clean')
 shell.exec('lerna clean --yes')
 shell.exec('rm -rf yarn.lock')
