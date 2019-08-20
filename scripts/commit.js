a534efcaebb2c82fb7bc6f952fad016fd68dda4c
Phuoc <s.phuoc.317049@gmail.com>
2019-08-20 15:13:44 +0800

    Update config

--- scripts/commit.js
+++ scripts/commit.js
@@ -4,4 +4,5 @@ const commitMessage = process.argv[3]
 
 shell.exec('git add .')
 shell.exec(`git commit -m "${commitMessage}"`)
+shell.exec('git push -f github')
 shell.exec('git push -f origin master')
